20f2ac867ed86ed2c6bf43162d67eb9e8a8b04d6
aryan21617 <aryanpurbey1617@gmail.com>
2025-08-22 01:23:44 +0530

    made delete button interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,7 @@ cart.forEach((cartItem) => {
     }
   });
   cartSummaryHTML +=`
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
@@ -102,7 +102,10 @@ document.querySelectorAll('.js-delete-quantity-link').forEach((link) => {
     const productId = link.dataset.productId;
     deleteCartItem(productId);
 
-    console.log(cart);
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    if (container) {
+      container.remove();
+    }
   });
 });
 
